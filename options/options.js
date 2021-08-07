7998df9c32bf32037390088fcbb638640ba8653d
Crowdbotics <team@crowdbotics.com>
2021-08-07 18:06:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "posttalk_29616Identifier",
+  url: "https://posttalk_29616Identifier.botics.co",
+  api: "https://posttalk_29616Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
